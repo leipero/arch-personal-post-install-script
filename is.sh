1ed876bb180c3e3a4534c06dee9de3f667a0d1bb
leipero <lei.pero@gmail.com>
2020-11-23 10:07:22 +0100

    Update is.sh

--- is.sh
+++ is.sh
@@ -7,8 +7,8 @@ sudo pacman -Syyu
 #Mesa (Open Source drivers, enable and change if needed)
 sudo pacman -S --noconfirm lib32-mesa mesa-demos libva-mesa-driver glu lib32-glu opencl-mesa
 #Packages / Paketi
-sudo pacman -S --noconfirm git ufw ffmpegthumbnailer gst-libav gst-plugins-base gst-plugins-good gtk-engine-murrine ntfs-3g p7zip unrar qt5ct youtube-dl mpv file-roller xorg-fonts-type1 acpid dosfstools gparted ttf-freefont ttf-dejavu ttf-sazanami ttf-fireflysung noto-fonts-emoji xorg-xlsfonts qt5-styleplugins transmission-gtk firefox firefox-i18n-sr chromium obs-studio wine-staging wine-nine materia-gtk-theme
-sudo pacman -S --noconfirm lib32-libpulse lib32-openal lib32-gnutls lib32-mpg123 lib32-libxml2 lib32-lcms2 lib32-giflib lib32-libpng lib32-alsa-lib lib32-alsa-plugins lib32-nss lib32-gtk2 lib32-gtk3 lib32-libcanberra lib32-dbus-glib lib32-libnm-glib lib32-libudev0-shim libpng12 lib32-libpng12 lib32-libcurl-gnutls lib32-libcurl-compat lib32-libxv lib32-ncurses lib32-sdl lib32-zlib lib32-libgcrypt lib32-libgcrypt15
+sudo pacman -S --noconfirm git ufw ffmpegthumbnailer gst-libav gst-plugins-base gst-plugins-good gtk-engine-murrine ntfs-3g p7zip unrar youtube-dl mpv file-roller xorg-fonts-type1 acpid dosfstools gparted ttf-freefont ttf-dejavu ttf-sazanami ttf-fireflysung noto-fonts-emoji xorg-xlsfonts transmission-gtk chromium obs-studio wine-staging wine-nine materia-gtk-theme
+#sudo pacman -S --noconfirm lib32-libpulse lib32-openal lib32-gnutls lib32-mpg123 lib32-libxml2 lib32-lcms2 lib32-giflib lib32-libpng lib32-alsa-lib lib32-alsa-plugins lib32-nss lib32-gtk2 lib32-gtk3 lib32-libcanberra lib32-dbus-glib lib32-libnm-glib lib32-libudev0-shim libpng12 lib32-libpng12 lib32-libcurl-gnutls lib32-libcurl-compat lib32-libxv lib32-ncurses lib32-sdl lib32-zlib lib32-libgcrypt lib32-libgcrypt15
 
 #fstab automount of device "sdxy" (enable-change for your device) / fstab kačenje uređaja "sdxy"
 #echo "/dev/sdb1               /media/sdb1     ext4            defaults                        0 2" | sudo tee -a /etc/fstab
@@ -19,20 +19,10 @@ echo "#CLUTTER_DEFAULT_FPS=120
 #R600_DEBUG=nodma
 #MESA_GL_VERSION_OVERRIDE=4.5
 #MESA_GLSL_VERSION_OVERRIDE=450
-CLUTTER_VBLANK=none
-QT_QPA_PLATFORMTHEME=qt5ct" | sudo tee -a /etc/environment
-
-#X.Org (NOT NEEDED ANYMORE)
-#echo 'Section "Device"
-#	Identifier "Radeon"
-#	Driver "radeon"
-#	Option "AccelMethod" "exa"
-#	Option "TearFree" "on"
-#	Option "DRI" "2"
-#EndSection' | sudo tee /etc/X11/xorg.conf.d/20-radeon.conf
+CLUTTER_VBLANK=none" | sudo tee -a /etc/environment
 
 #Virtual memory (default "60", "0" = disabled) / Virtuelna memorija (podrazumevano "60")
-#echo "vm.swappiness=0" | sudo tee /etc/sysctl.d/99-sysctl.conf
+echo "vm.swappiness=0" | sudo tee /etc/sysctl.d/99-sysctl.conf
 
 #Number of cores for AUR (-j3 = 2 cores) / Broj jezgara za AUR (-j3 = 2 jezgra)
 #Change "-j5" to your number of cores + 1
@@ -40,8 +30,8 @@ sudo sed -i -e 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j5"/g' /etc/makepkg.conf
 sudo sed -i -e 's/#PACKAGER="John Doe <john@doe.com>"/PACKAGER="lpr1"/g' /etc/makepkg.conf
 
 #AUR: Installation of "yay" package / Instalacija "yay" paketa
-git clone https://aur.archlinux.org/yay.git
-cd ./yay
+git clone https://aur.archlinux.org/yay-bin.git
+cd ./yay-bin
 makepkg -sic --noconfirm
 
 #Reserved EXT4 space / Rezervisan EXT4 prostor
@@ -51,20 +41,16 @@ makepkg -sic --noconfirm
 #Enable TRIM support / Omogući TRIM podršku
 sudo systemctl enable fstrim.timer
 
-#GDM onemogući "wayland" / GDM disable "wayland"
-#sudo sed -i -e 's/#WaylandEnable=false/WaylandEnable=false/g' /etc/gdm/custom.conf
-
 #Uncomplicated firewall / Zaštitni zid
 sudo ufw default deny
 sudo ufw allow SSH
-sudo ufw allow transmision
+sudo ufw allow transmission
 sudo ufw enable
 sudo systemctl enable ufw
 
 #DNS network settings (systemd) / DNS mrežna podešavanja
 echo "[main]
-dns=systemd-resolved" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
-sudo systemctl enable systemd-networkd.service
+dns=systemd-resolved" | sudo tee -a /etc/NetworkManager/conf.d/dns.conf
 sudo systemctl enable systemd-resolved.service
 
 #Silent boot / Tiho pokretanje
@@ -126,29 +112,21 @@ echo '<driconf>
 gsettings set org.gnome.desktop.interface clock-show-date 'true'
 gsettings set org.gnome.desktop.interface gtk-theme 'Materia-light'
 gsettings set org.gnome.desktop.interface icon-theme 'Numix-Circle'
-gsettings set org.gnome.desktop.media-handling autorun-never 'true'
 gsettings set org.gnome.desktop.privacy remember-recent-files 'false'
 gsettings set org.gnome.desktop.screensaver lock-enabled 'false'
 gsettings set org.gnome.desktop.search-providers disable-external 'true'
-gsettings set org.gnome.desktop.session idle-delay '1200'
 gsettings set org.gnome.desktop.wm.preferences action-middle-click-titlebar 'none'
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
 gsettings set org.gnome.Epiphany homepage-url 'about:blank'
 gsettings set org.gnome.Epiphany restore-session-policy 'crashed'
 gsettings set org.gnome.gnome-system-monitor current-tab 'resources'
 gsettings set org.gnome.mutter center-new-windows 'true'
-gsettings set org.gnome.nautilus.window-state sidebar-width '190'
 gsettings set org.gnome.nautilus.preferences executable-text-activation 'ask'
-gsettings set org.gnome.nautilus.window-state initial-size '(848, 586)'
 gsettings set org.gnome.desktop.peripherals.keyboard remember-numlock-state 'true'
 gsettings set org.gnome.desktop.peripherals.keyboard numlock-state 'true'
-gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar 'false'
-gsettings set org.gnome.shell enabled-extensions "['alternate-tab@gnome-shell-extensions.gcampax.github.com','user-theme@gnome-shell-extensions.gcampax.github.com']"
+gsettings set org.gnome.shell enabled-extensions "['user-theme@gnome-shell-extensions.gcampax.github.com']"
 gsettings set org.gnome.shell.extensions.user-theme name 'Materia-light'
-gsettings set org.gtk.Settings.FileChooser sidebar-width '190'
 gsettings set org.gtk.Settings.FileChooser sort-directories-first 'true'
-gsettings set org.gtk.Settings.FileChooser window-position '(574, 231)'
-gsettings set org.gtk.Settings.FileChooser window-size '(773, 570)'
 
 #Terminal/bashrc
 echo "#
@@ -173,10 +151,10 @@ slang=sr,en,eng
 sub-codepage=utf8:cp1250
 #sub-codepage=utf8:cp1251
 loop-playlist=inf
-#vo=gpu
+vo=xv
 #hwdec=auto
 #hwdec-codecs=all
-#mute=yes" > ~/.mpv/mpv.conf
+mute=yes" > ~/.mpv/mpv.conf
 echo "MOUSE_BTN3 add volume 5
 MOUSE_BTN4 add volume -5" > ~/.mpv/input.conf
 
@@ -200,15 +178,16 @@ sudo glib-compile-resources --target=/usr/share/gnome-shell/gnome-shell-theme.gr
 #Auto clean package cache (paccache -h) / Automatsko čišćenje keša paketa (paccache -h)
 sudo mkdir /etc/pacman.d/hooks
 echo "[Trigger]
-Operation = Upgrade
-Operation = Install
 Operation = Remove
+Operation = Install
+Operation = Upgrade
 Type = Package
 Target = *
+
 [Action]
-Description = Cleaning pacman cache...
+Description = Removing unnecessary cached files…
 When = PostTransaction
-Exec = /usr/bin/paccache -ruk0" | sudo tee -a /usr/share/libalpm/hooks/cleanup.hook
+Exec = /usr/bin/paccache -rk0" | sudo tee -a /usr/share/libalpm/hooks/cleanup.hook
 
 unset LC_ALL
 
